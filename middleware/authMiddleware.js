dc7d7ea277b32a3421af305e591dea996143d0d6
vitay <vitaykosw@gmail.com>
2024-09-17 18:20:29 +0530

    Job Posting Authorization

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,21 +1,32 @@
 import jwt from "jsonwebtoken";
-const authenticateToken=(req,res,next)=>{
-    const authHeader=req.header('Authorization');
-    const token=authHeader && authHeader.startsWith('Bearer ')?authHeader.split(' ')[1]:null;
-    if(!token){
-        return res.status(401).json({message:'Access denied. No token provided.'});
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.header('Authorization');
+    const token = authHeader && authHeader.startsWith('Bearer ') ? authHeader.split(' ')[1] : null;
+    if (!token) {
+        return res.status(401).json({ message: 'Access denied. No token provided.' });
     }
 
-    try{
-        const decoded=jwt.verify(token, process.env.SECRET_KEY);
-        req.user=decoded;
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        req.user = decoded;
         next();
-
-    }catch(error){
-        return res.status(401).json({message:'Invalid Token'});
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid Token' });
     }
+};
 
+const checkRole = (role) => {
+    return (req, res, next) => {
+        try {
+            if (req.user.role !== role) {
+                return res.status(403).json({ message: 'Access denied. Insufficient Permission' });
+            }
+            next(); 
+        } catch (error) {
+            return res.status(500).json({ message: 'Internal Server Error' });
+        }
+    };
+};
 
-}
-
-export default authenticateToken
+export { authenticateToken, checkRole };
--- routes/jobRoute.js
+++ routes/jobRoute.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { getJobDetails, postJob } from "../controllers/jobController.js";
-import authenticateToken from "../middleware/authMiddleware.js";
+import { authenticateToken, checkRole } from "../middleware/authMiddleware.js";
 
-const router=Router();
-router.post("/postjob",authenticateToken,postJob);
-router.get("/jobs",authenticateToken,getJobDetails);
-export default router; 
+const router = Router();
+
+router.post("/postjob", authenticateToken, checkRole('Company'), postJob);
+
+router.get("/jobs", authenticateToken, getJobDetails);
+
+export default router;
